03068a02886790b42f47214b7efbd20194183a11
Fabian Astudillo Salinas <fabian.astudillo.salinas@gmail.com>
2025-06-04 19:11:12 -0500

    Adding install.jl

---
+++ chap1-introduccion/install.jl
@@ -0,0 +1,9 @@
+import Pkg;
+#Pkg.add("Graphs");
+#Pkg.add("GraphPlot");
+#Pkg.add("Plots")
+#Pkg.add("CairoMakie")
+#Pkg.add("IJulia")
+#Pkg.add("Compose")
+#Pkg.add("Measures")
+Pkg.add("GraphIO")
