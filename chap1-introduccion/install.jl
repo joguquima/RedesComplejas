ddde72d71ec71a577754874556ec8fd6acce7783
Fabian Astudillo Salinas <fabian.astudillo.salinas@gmail.com>
2025-06-10 18:17:06 -0500

    Adding Example Red Dirigida

--- chap1-introduccion/install.jl
+++ chap1-introduccion/install.jl
@@ -1,9 +1,12 @@
 import Pkg;
-#Pkg.add("Graphs");
-#Pkg.add("GraphPlot");
-#Pkg.add("Plots")
-#Pkg.add("CairoMakie")
-#Pkg.add("IJulia")
-#Pkg.add("Compose")
-#Pkg.add("Measures")
+Pkg.add("Graphs");
+Pkg.add("GraphPlot");
+Pkg.add("Plots")
+Pkg.add("CairoMakie")
+Pkg.add("IJulia")
+Pkg.add("Compose")
+Pkg.add("Measures")
 Pkg.add("GraphIO")
+Pkg.add("CSV")
+Pkg.add("DataFrames")
+Pkg.add("Compose")
---
+++ chap1-introduccion/reddirigida.jl
@@ -0,0 +1,12 @@
+using LightGraphs, SparseArrays
+
+# Crear grafo dirigido
+g = SimpleDiGraph(5)
+add_edge!(g, 1, 2)
+add_edge!(g, 1, 3)
+add_edge!(g, 2, 4)
+add_edge!(g, 3, 5)
+
+# Obtener matriz de adyacencia
+A = adjacency_matrix(g)
+display(Matrix(A))
